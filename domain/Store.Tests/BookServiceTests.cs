5a967014682a7e06c36f7c6a45ac9832fe2b765f
ovchinnikov-26-98 <ovchinnikov.26.98@mail.ru>
2022-07-26 02:21:47 +0300

    реализовать поиск по ISBN и автору

---
+++ domain/Store.Tests/BookServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Store.Tests
+{
+    public class BookServiceTests
+    {
+        [Fact]
+        public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
+        {
+            var bookRepositoryStub = new Mock<IBookRepository>();
+            bookRepositoryStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>()))
+                .Returns(new[] { new Book(1, "", "", "") });
+
+            bookRepositoryStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>()))
+                .Returns(new[] { new Book(2, "", "", "") });
+
+            var bookServise = new BookService(bookRepositoryStub.Object);
+
+            var actual = bookServise.GetAllByQuery("ISBN 12345-67890");
+
+            Assert.Collection(actual, book => Assert.Equal(1, book.Id));
+        }
+
+        [Fact]
+        public void GetAllByQuery_WithAuthor_CallsGetAllByTitleOrAuthor()
+        {
+            var bookRepositoryStub = new Mock<IBookRepository>();
+            bookRepositoryStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>()))
+                .Returns(new[] { new Book(1, "", "", "") });
+
+            bookRepositoryStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>()))
+                .Returns(new[] { new Book(2, "", "", "") });
+
+            var bookServise = new BookService(bookRepositoryStub.Object);
+
+            var actual = bookServise.GetAllByQuery("12345-67890");
+
+            Assert.Collection(actual, book => Assert.Equal(2, book.Id));
+        }
+    }
+}
