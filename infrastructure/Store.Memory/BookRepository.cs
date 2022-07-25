4e5c1754a5371a3c7e509a2167bc281a00571d8e
ovchinnikov-26-98 <ovchinnikov.26.98@mail.ru>
2022-07-25 23:27:22 +0300

    Создать веб приложение

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,7 +1,16 @@
 ﻿namespace Store.Memory
 {
-    public class Class1
+    public class BookRepository : IBookRepository
     {
-
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language")
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
     }
 }
