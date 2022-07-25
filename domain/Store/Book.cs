ffd1d75ecffeb1a472eac293c96ee1f721d2ca1b
ovchinnikov-26-98 <ovchinnikov.26.98@mail.ru>
2022-07-25 23:18:37 +0300

    добавить реализацию репозитория книг в памяти

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,5 +1,16 @@
-﻿namespace Store;
-public class Class1
-{
+﻿using System;
 
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Title { get; }
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
 }
+
