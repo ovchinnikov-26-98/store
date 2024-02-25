99d9bb20c184c192e9ad1e9a93d40bfb3915b94c
ovchinnikov-26-98 <ovchinnikov.26.98@mail.ru>
2024-02-25 20:41:20 +0300

    добавить реализацию интерфейса репозитория книг нв памяти

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Store
 {
-    public interface IBookRepositorycs
+    public interface IBookRepository
     {
         Book[] GetAllByTitle(string titlePart)
     }
